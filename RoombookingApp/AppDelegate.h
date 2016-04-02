bc59a3658e5833d6d7151003ece13fbdfc89334f
Mohamed Alsadek <mohamedalsadek@Mohameds-MacBook-Pro.local>
2016-04-02 21:36:42 +0200

    initial commit

---
+++ RoombookingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoombookingApp
+//
+//  Created by Mohamed Alsadek on 4/2/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoombookingApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RoombookingApp
+//
+//  Created by Mohamed Alsadek on 4/2/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
